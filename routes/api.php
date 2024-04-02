40e39bfe0b4b8c7229d44622f169ce79937c4d07
jhonsuarez18 <suarez.ortiz.john@gmail.com>
2024-04-02 16:29:33 -0500

    se agrego rutas y resources

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
+use  App\Http\Controllers\Api\ApiEPDetallePedidoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +12,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+/// seccion apis
+/// apis pedidos
+Route::get('/apipedido', 'EpPedidoController@getPedidoItemsBS');
+Route::get('/pedidomontoact/{idped}/{mont}', 'EpPedidoController@updatePedidoSinMonto');
+
+//
+
+Route::apiResource('/appedido', ApiEPDetallePedidoController::class);
+
+Route::post('/apiitmpedido', 'EPDetallePedidoController@store');
+
+Route::post('/apidetpedido', 'EpPedidoController@update');
+
+Route::get('/apiactmontPed', 'EpPedidoController@actMontPed');
+
+Route::get('test',function(){
+    return response([1,2,3,4],200);
 });
